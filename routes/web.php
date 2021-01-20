58cf750c3d6e1ebb53512676968cd4f31e378a5e
Marco Guidi <mark.88g@gmail.com>
2021-01-20 15:07:27 +0100

    + create HomeController % mod rotte

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index') -> name('home_page');
 
 Route::resource('/products', 'ProductController');
