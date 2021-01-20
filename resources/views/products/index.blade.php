4c704765e4a52f2b13a3806b68e1881845fdcd82
Marco Guidi <mark.88g@gmail.com>
2021-01-20 15:57:07 +0100

    % products.index inserita tab statica

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -9,5 +9,33 @@
                 <h1>All products</h1>
             </div>
         </div>
+
+        <div class="row">
+            <div class="col-12">
+                <table class="table">
+                    {{-- intestazione --}}
+                    <thead>
+                        <tr class="thead-dark">
+                            <th scope="col">id</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Brand</th>
+                            <th scope="col">Note</th>
+                            <th scope="col">Price</th>
+                        </tr>
+                    </thead>
+
+                    {{-- corpo --}}
+                    <tbody>
+                        <tr>
+                            <td>Mark</td>
+                            <td>Mark</td>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
 @endsection
