db3b028f82d553341b2608c50a3c5d1685a615df
Marco Guidi <mark.88g@gmail.com>
2021-01-20 15:52:30 +0100

    + rotta products -> index + index.blade

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('title', 'Products | All products')
+
+@section('main-content')
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1>All products</h1>
+            </div>
+        </div>
+    </div>
+@endsection
