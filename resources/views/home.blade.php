44a76e3acefd6de0ea57b7ebc212c86272d7e800
Marco Guidi <mark.88g@gmail.com>
2021-01-20 15:15:10 +0100

    -canc pg welcome laravel +agg pg home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('title', 'Products | Homepage')
+
+@section('main-content')
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1>Homepage</h1>
+            </div>
+        </div>
+    </div>
+@endsection
